b195ddb06be77007ea64eef4ac1f095e967aa743
Susanna <s.s.s.84@yandex.ru>
2024-06-13 13:42:23 +0300

    Second project

--- tests/tests_hw/test_visible_hw.py
+++ tests/tests_hw/test_visible_hw.py
@@ -1,16 +1,19 @@
-from pages.accordion import Accordion
-import time
+from pages.accordion import Accordion #наследование класса Accordion для доступа к сайту https://demoqa.com/accordian
+import time # для использования времени при тестировании
 
-def test_accordion(browser):
-    accordion_page = Accordion(browser)
-    accordion_page.visit()
+def test_accordion(browser):# получение доступа к браузеру
+    accordion_page = Accordion(browser) #перейти на страницу https://demoqa.com/accordian
+    accordion_page.visit()# посетить страницу
 
-    assert accordion_page.Lorem_Ipsum_text.visible()
+    assert accordion_page.Lorem_Ipsum_text.visible() # проверка на видимость текста
 
-    accordion_page.icon_check.click()
-    time.sleep(2)
-    assert accordion_page.icon_check.visible()
-    # assert not accordion_page.icon_check.visible()
+def test_accordion_click(browser):
+    accordion_page = Accordion(browser)  # перейти на страницу https://demoqa.com/accordian
+    accordion_page.visit()  # посетить страницу
+    accordion_page.icon_check.click()#проверка на кликабельность элемента
+    time.sleep(3)# задержка времени для проверки
+    assert accordion_page.icon_check.visible()#проверка видимости элемента
+    assert not accordion_page.Lorem_Ipsum_text.visible()#проверка на невидимость текста
 
 
 def test_visible_accordion_default(browser):
