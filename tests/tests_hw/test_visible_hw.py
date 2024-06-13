13d1a4277a682f08139f500c952803161e6c7451
Susanna <s.s.s.84@yandex.ru>
2024-06-13 13:43:48 +0300

    Second project

--- tests/tests_hw/test_visible_hw.py
+++ tests/tests_hw/test_visible_hw.py
@@ -21,8 +21,8 @@ def test_visible_accordion_default(browser):
     accordion_page = Accordion(browser)
     accordion_page.visit()
     # assert accordion_page.section_text1_check.visible()
-    assert not accordion_page.section_text1_check.visible()
+    assert not accordion_page.section_text1_check.visible()#проверка на невидимость текста
     # assert accordion_page.section_text2_check.visible()
-    assert not accordion_page.section_text2_check.visible()
+    assert not accordion_page.section_text2_check.visible()#проверка на невидимость текста
     # assert accordion_page.section_text3_check.visible()
-    assert not accordion_page.section_text3_check.visible()
+    assert not accordion_page.section_text3_check.visible()#проверка на невидимость текста
